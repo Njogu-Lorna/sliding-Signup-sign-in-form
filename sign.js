3447f05d83238f681e08dffc11a30acc5e643b98
Smiley-Joker <65446528+Smiley-Joker@users.noreply.github.com>
2020-08-24 20:08:49 +0300

    -

---
+++ sign.js
@@ -0,0 +1,10 @@
+const signUpButton = document.getElementById('SignUp');
+const signUpButton = document.getElementById('SignIn');
+const signUpButton = document.getElementById('container');
+
+signUpButton.addEventListener('onclick', () =>
+container.classList.add('right-panel-active'));
+
+signInButton.addEventListener('conlick',() =>
+container.classList.remove('right-panel-active')
+);
