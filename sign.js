15b4eebfc47557239b397b1be11602377a16ff30
collinskoech11 <collinskoechck34@gmail.com>
2020-09-01 21:14:28 +0300

    styling update

--- sign.js
+++ sign.js
@@ -1,11 +1,11 @@
-const signUpButton = document.getElementById('SignUp');
-const signInButton = document.getElementById('SignIn');
 const container = document.getElementById('container');
-
-signUpButton.addEventListener('click',() =>
-container.classList.add('right-panel-active')
-);
-
-signInButton.addEventListener('click',() =>
-container.classList.remove('right-panel-active')
-);
+document.querySelector('#SignUp').addEventListener('click',
+	function(){
+		container.classList.toggle('right-panel-active');
+		container.removeClass('right-panel-active');
+	});
+document.querySelector('#SignIn').addEventListener('click',
+	function(){
+		container.classList.toggle('right-panel-active');
+		container.removeClass('right-panel-active');
+	});
